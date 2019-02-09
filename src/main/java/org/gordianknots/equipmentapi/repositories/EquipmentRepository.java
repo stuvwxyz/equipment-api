dcd1162411976adf8fb90b9ec4cff3d18cd78e18
Tony <Tony.Wolyniec@Target.com>
2019-02-08 18:24:19 -0600

    Sorted find all by equipment ID

--- src/main/java/org/gordianknots/equipmentapi/repositories/EquipmentRepository.java
+++ src/main/java/org/gordianknots/equipmentapi/repositories/EquipmentRepository.java
@@ -3,5 +3,8 @@ package org.gordianknots.equipmentapi.repositories;
 import org.gordianknots.equipmentapi.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+    public List<Equipment> findAllByOrderByEquipmentId();
 }
